a78418bee981357fb47878b8eeb8b9cf36cc100b
ShubhamWdOrg <83825493+ShubhamWdOrg@users.noreply.github.com>
2021-12-14 11:48:42 +0530

    delete old commit

--- 29-nov-2021/destroy.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header("location:login.php?mes=<p> <u></u><b>You are logout.Please Login Here</b></u></p>");
-
-?>
--- admin -fp/destroy.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header("location:index.php?mes=<p> <u></u><b>You are logout.Please Login Here</b></u></p>");
-
-?>
